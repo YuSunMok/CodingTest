54b7cb49705a3a4cba450286955863109507ab84
sunmok <tjsahr9191@naver.com>
2023-11-09 21:49:27 +0900

    자바 알고리즘 문제풀이 입문>문자열>3.문장 속 단어

---
+++ src/Introduction_to_Java_algorithm/String/Words_In_A_Sentence.java
@@ -0,0 +1,45 @@
+package Introduction_to_Java_algorithm.String;
+
+import java.util.*;
+
+public class Words_In_A_Sentence {
+    public static void main(String[] args) {
+        Words_In_A_Sentence T = new Words_In_A_Sentence();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(T.solution2(str));
+    }
+
+    public String solution(String str) {
+        String answer = "";
+
+        int max = Integer.MIN_VALUE;
+        int pos = 0;
+
+        while ((pos = str.indexOf(" ")) != -1) {
+            String temp = str.substring(0, pos);
+            int len = temp.length();
+            if (len > max) {
+                max = len;
+                answer = temp;
+            }
+            str = str.substring(pos+1);
+        }
+        if(str.length() > max) answer = str;
+        return answer;
+    }
+
+    public String solution2(String str) {
+        String answer = "";
+        int max = Integer.MIN_VALUE;
+        String[] strArr = str.split(" ");
+        for (String s : strArr) {
+            int len = s.length();
+            if (len > max) {
+                max = len;
+                answer = s;
+            }
+        }
+        return answer;
+    }
+}
