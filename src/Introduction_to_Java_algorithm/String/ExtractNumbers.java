7341e89abae3e551756bf5425612f8d69e5e434e
sunmok <tjsahr9191@naver.com>
2023-11-17 10:55:00 +0900

    자바 알고리즘 문제풀이 입문>문자열>9.숫자만 추출

---
+++ src/Introduction_to_Java_algorithm/String/ExtractNumbers.java
@@ -0,0 +1,17 @@
+package Introduction_to_Java_algorithm.String;
+
+import java.util.Scanner;
+
+public class ExtractNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String answer = "";
+        String str = sc.next();
+        for (int i = 0; i < str.length(); i++) {
+            if(Character.isDigit(str.charAt(i))) {
+                answer += str.charAt(i);
+            }
+        }
+        System.out.println(Integer.parseInt(answer));
+    }
+}
