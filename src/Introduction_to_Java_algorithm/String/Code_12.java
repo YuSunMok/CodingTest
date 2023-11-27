3e12050b99f12a129cb37422e40fbcb9c0d17856
sunmok <tjsahr9191@naver.com>
2023-11-27 23:54:20 +0900

    자바 알고리즘 문제풀이 입문>문자열>12. 암호

---
+++ src/Introduction_to_Java_algorithm/String/Code_12.java
@@ -0,0 +1,48 @@
+package Introduction_to_Java_algorithm.String;
+
+import java.util.Scanner;
+
+public class Code_12 {
+    public static void main(String[] args) {
+        mySolution();
+        solution();
+    }
+
+    private static void solution() {
+        Scanner sc = new Scanner(System.in);
+        int len = Integer.parseInt(sc.next());
+        String s = sc.next();
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < len; i++) {
+            String temp = s.substring(0, 7).replace("#", "1").replace("*", "0");
+            int num = Integer.parseInt(temp, 2);
+            sb.append((char) num);
+            s = s.substring(7);
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    private static void mySolution() {
+        Scanner sc = new Scanner(System.in);
+        int len = Integer.parseInt(sc.next());
+        String s = sc.next();
+        s = s.replace("#", "1");
+        s = s.replace("*", "0");
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < len; i++) {
+            String str = new StringBuilder(s.substring(0, 7)).reverse().toString();
+            int result = 0;
+            for(int j = 0; j < str.length(); j++) {
+                if(str.charAt(j) == '1') {
+                    result += Math.pow(2, j);
+                }
+            }
+            sb.append((char)result);
+            s = s.substring(7);
+        }
+        System.out.println(sb.toString());
+    }
+}
