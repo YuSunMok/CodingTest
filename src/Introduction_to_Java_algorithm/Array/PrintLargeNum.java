d9f33c01c554d64f891ff2cdaf6fec1f5094114c
sunmok <tjsahr9191@naver.com>
2023-11-28 17:51:09 +0900

    자바 알고리즘 문제풀이 입문>1,2차원 배열>1. 큰 수 출력하기

---
+++ src/Introduction_to_Java_algorithm/Array/PrintLargeNum.java
@@ -0,0 +1,20 @@
+package Introduction_to_Java_algorithm.Array;
+
+import java.util.Scanner;
+
+public class PrintLargeNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.next());
+        int min = Integer.MIN_VALUE;
+        String answer = "";
+        for(int i = 0; i < n; i++) {
+            int temp = Integer.parseInt(sc.next());
+            if(temp > min) {
+                answer += temp + " ";
+            }
+            min = temp;
+        }
+        System.out.println(answer);
+    }
+}
