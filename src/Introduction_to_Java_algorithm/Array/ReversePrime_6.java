ea81cf32065284c8e99a14b9e3dc06a0c457abfa
sunmok <tjsahr9191@naver.com>
2023-12-02 13:11:59 +0900

    자바 알고리즘 문제풀이 입문>1,2차원 배열>6. 뒤집은 소수

---
+++ src/Introduction_to_Java_algorithm/Array/ReversePrime_6.java
@@ -0,0 +1,58 @@
+package Introduction_to_Java_algorithm.Array;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ReversePrime_6 {
+    public static void main(String[] args) {
+        mySolution();
+        solution();
+    }
+
+    private static void solution() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for(int i = 0; i < n; i++) {
+            int temp = arr[i];
+            int res = 0;
+            while(temp > 0) {
+                int t = temp % 10;
+                res = res * 10 + t;
+                temp /= 10;
+            }
+            if (isPrime(res)) {
+                System.out.print(res + " ");
+            }
+        }
+    }
+
+    private static void mySolution() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < n; i++) {
+            String input = sc.next();
+            int num = Integer.parseInt(sb.append(input).reverse().toString());
+            if(isPrime(num)) {
+                System.out.print(num + " ");
+            }
+            sb.delete(0, input.length());
+        }
+    }
+
+    public static boolean isPrime(int num){
+        if(num == 1) return false;
+        for(int i = 2; i < num/2; i++) {
+            if(num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
