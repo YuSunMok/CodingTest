626a5ebc38558de4e50c65843531d4c4a7ace7d1
sunmok <tjsahr9191@naver.com>
2023-12-06 17:52:47 +0900

    자바 알고리즘 문제풀이 입문>1,2차원 배열>8. 등수 구하기

---
+++ src/Introduction_to_Java_algorithm/Array/FindRank_8.java
@@ -0,0 +1,29 @@
+package Introduction_to_Java_algorithm.Array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FindRank_8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] scores = new int[n];
+
+        for(int i = 0; i < n ; i++) {
+            scores[i] = sc.nextInt();
+        }
+
+        int[] ranks = new int[n];
+        for(int i = 0; i < n ; i++) {
+            int cnt = 1;
+            for (int j = 0; j < n; j++) {
+                if(scores[i] < scores[j]) {
+                    cnt++;
+                }
+            }
+            ranks[i] = cnt;
+        }
+
+        Arrays.stream(ranks).forEach(i -> System.out.print(i + " "));
+    }
+}
