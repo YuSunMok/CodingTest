a317d6a0f3b1d5b6a9fd4513651f0b2143e92bc8
sunmok <tjsahr9191@naver.com>
2023-12-09 14:56:25 +0900

    자바 알고리즘 문제풀이 입문>1,2차원 배열>9. 격자판 최대합

---
+++ src/Introduction_to_Java_algorithm/Array/gridMaxSum_9.java
@@ -0,0 +1,38 @@
+package Introduction_to_Java_algorithm.Array;
+
+import java.util.Scanner;
+
+public class gridMaxSum_9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+        for(int i = 0; i < n ; i++) {
+            for(int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        int sum1 = 0;
+        int sum2 = 0;
+        int result = Integer.MIN_VALUE;
+        for(int i = 0; i < n ; i++) {
+            sum1 = sum2 = 0;
+            for(int j = 0; j < n ; j++) {
+                sum1 += arr[i][j];
+                sum2 += arr[j][i];
+            }
+        }
+        result = Math.max(sum1, result);
+        result = Math.max(sum2, result);
+        sum1 = sum2 = 0;
+        for(int i = 0; i < n ; i++) {
+            sum1 += arr[i][i];
+            sum2 += arr[i][n-1-i];
+        }
+        result = Math.max(sum1, result);
+        result = Math.max(sum2, result);
+
+        System.out.println(result);
+    }
+}
