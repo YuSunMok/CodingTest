b6a50b7e6b9d7eb54d758f025d1bceab9c46d3fb
sunmok <tjsahr9191@naver.com>
2023-12-19 23:10:30 +0900

    자바 알고리즘 문제풀이 입문>1,2차원 배열>11.임시반장정하기

---
+++ src/Introduction_to_Java_algorithm/Array/FindTemporaryTeamLeader_11.java
@@ -0,0 +1,36 @@
+package Introduction_to_Java_algorithm.Array;
+
+import java.util.Scanner;
+
+public class FindTemporaryTeamLeader_11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][5];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 5; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        int max = Integer.MIN_VALUE;
+        int maxIdx = 0;
+        for (int i = 0; i < n; i++) {
+            int cnt = 0;
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < 5; k++) {
+                    if (arr[i][k] == arr[j][k]) {
+                        cnt++;
+                        break;
+                    }
+                }
+            }
+            if (cnt > max) {
+                max = cnt;
+                maxIdx = i;
+            }
+        }
+        System.out.println(maxIdx + 1);
+
+    }
+}
