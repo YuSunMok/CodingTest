42254ebaa136187775e9220cd62718525b43e518
sunmok <tjsahr9191@naver.com>
2023-12-20 22:44:58 +0900

    자바 알고리즘 문제풀이 입문>1,2차원 배열>12.멘토링

---
+++ src/Introduction_to_Java_algorithm/Array/Mentoring_12.java
@@ -0,0 +1,38 @@
+package Introduction_to_Java_algorithm.Array;
+
+import java.util.Scanner;
+
+public class Mentoring_12 {
+    public static void main (String[] args) throws java.lang.Exception 	{
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // 학생수
+        int m = sc.nextInt(); // 문제수
+        int[][] arr = new int[m][n];
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        int result = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                int cnt = 0;
+                for (int k = 0; k < m; k++) {
+                    int pi = 0;
+                    int pj = 0;
+                    for (int l = 0; l < n; l++) {
+                        if(arr[k][l] == i) pi = l;
+                        if(arr[k][l] == j) pj = l;
+                    }
+                    if(pi < pj) cnt++;
+                }
+                if (cnt == m) {
+                    result++;
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+}
