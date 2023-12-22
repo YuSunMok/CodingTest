2e42fb7c01cc9b6c8e57a941512833234e0c0904
sunmok <tjsahr9191@naver.com>
2023-12-22 14:07:31 +0900

    자바 알고리즘 문제풀이 입문>twopointers>2.공통원소 구하기

--- src/twopointers/CombineTwoArrays_1.java
+++ src/twopointers/CombineTwoArrays_1.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
-public class CombineTwoArrays {
+public class CombineTwoArrays_1 {
     public static void main(String[] args) {
         solution();
         mySolution();
---
+++ src/twopointers/InterSection_2.java
@@ -0,0 +1,38 @@
+package twopointers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class InterSection_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        int m = sc.nextInt();
+        int[] b = new int[m];
+        for (int i = 0; i < m; i++) {
+            b[i] = sc.nextInt();
+        }
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+        List<Integer> list = new ArrayList<>();
+        int p1 = 0, p2 = 0;
+        while (p1 < n && p2 < m) {
+            if (a[p1] == b[p2]) {
+                list.add(a[p1++]);
+                p2++;
+            } else if (a[p1] < b[p2]) {
+                p1++;
+            } else {
+                p2++;
+            }
+        }
+        list.forEach(integer -> System.out.print(integer + " "));
+    }
+}
