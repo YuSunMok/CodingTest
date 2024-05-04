7c1bbed043cad7926e840633029a8db8975f30e7
sunmok <tjsahr9191@naver.com>
2024-05-04 11:14:31 +0900

    자바 알고리즘 문제풀이 입문>HashMap, TreeSet>2.아나그램

---
+++ src/Introduction_to_Java_algorithm/hashmap_treeset/anagram_2.java
@@ -0,0 +1,53 @@
+package Introduction_to_Java_algorithm.hashmap_treeset;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class anagram_2 {
+    public static void main(String[] args) {
+        solution();
+        mySolution();
+    }
+
+    private static void solution() {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.next();
+        String s2 = sc.next();
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char x : s1.toCharArray()) {
+            map.put(x, map.getOrDefault(x, 0) + 1);
+        }
+        String result = "YES";
+        for (char x : s2.toCharArray()) {
+            if (!map.containsKey(x) || map.get(x) == 0) {
+                result = "NO";
+                break;
+            }
+            map.put(x, map.get(x) - 1);
+        }
+        System.out.println(result);
+    }
+
+    private static void mySolution() {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.next();
+        String s2 = sc.next();
+        Map<Character, Integer> map1 = new HashMap<>();
+        Map<Character, Integer> map2 = new HashMap<>();
+        for(char x : s1.toCharArray()) {
+            map1.put(x, map1.getOrDefault(x, 0) + 1);
+        }
+        for(char x : s2.toCharArray()) {
+            map2.put(x, map2.getOrDefault(x, 0) + 1);
+        }
+        String result = "YES";
+        for(char x : s1.toCharArray()) {
+            if(map1.get(x) != map2.get(x)) {
+                result = "NO";
+                break;
+            }
+        }
+        System.out.println(result);
+    }
+}
