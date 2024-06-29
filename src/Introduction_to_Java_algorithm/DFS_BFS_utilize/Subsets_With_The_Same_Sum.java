5a4756e73ba4de1f39e0b3b4b45bbfe2cc837044
sunmok <tjsahr9191@naver.com>
2024-06-29 11:44:41 +0900

    자바 알고리즘 문제풀이 입문>DFS, BFS 활용>1.합이 같은 부분집합

---
+++ src/Introduction_to_Java_algorithm/DFS_BFS_utilize/Subsets_With_The_Same_Sum.java
@@ -0,0 +1,35 @@
+package Introduction_to_Java_algorithm.DFS_BFS_utilize;
+
+import java.util.Scanner;
+
+public class Subsets_With_The_Same_Sum {
+    static String answer = "NO";
+    static boolean flag = false;
+    static int n, total = 0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            total += arr[i];
+        }
+        DFS(0, 0, arr);
+        System.out.print(answer);
+    }
+
+    public static void DFS(int L, int sum, int[] arr) {
+        if(flag) return;
+        if(sum > total/2) return;
+        if(n == L) {
+            if((total - sum) == sum) {
+                answer = "YES";
+                flag = true;
+            }
+        } else {
+            DFS(L+1, sum+arr[L], arr);
+            DFS(L+1, sum, arr);
+        }
+    }
+}
